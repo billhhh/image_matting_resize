bb8b461f555bbdc6a6106b646d467268d24ec053
Bill Wang <billhhh@users.noreply.github.com>
2017-03-15 14:33:01 +0800

    Add files via upload

--- Downsize_avr.m
+++ Downsize_avr.m
@@ -5,7 +5,8 @@ function DszImg = Downsize_avr (IOrg)
            for i = 1:2:row
               for j = 1:2:col
                   for k = 1:layer
-                      ResImg((i+1)/2,(j+1)/2,k) = IOrg(i,j,k)+IOrg(i,j+1,k)+IOrg(i+1,j,k)+IOrg(i+1,j+1,k);
+                      pixel_avr = (IOrg(i,j,k)+IOrg(i,j+1,k)+IOrg(i+1,j,k)+IOrg(i+1,j+1,k))/4;
+                      ResImg((i+1)/2,(j+1)/2,k) = pixel_avr;
                   end
               end
            end
