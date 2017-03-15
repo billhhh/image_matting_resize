0d824707c7955ffc80159c97acec560a71dfd4b2
Bill Wang <billhhh@users.noreply.github.com>
2017-03-15 11:17:59 +0800

    Add files via upload

--- Downsize.m
+++ Downsize.m
@@ -1,12 +1,10 @@
 function DszImg = Downsize (IOrg)
 
            [row,col,layer] = size(IOrg);
-           %ResImg = [];
            
            for i = 1:2:row
               for j = 1:2:col
                   for k = 1:layer
-                      %fprintf('i: %d\n',i);
                       ResImg((i+1)/2,(j+1)/2,k) = IOrg(i,j,k);
                   end
               end
