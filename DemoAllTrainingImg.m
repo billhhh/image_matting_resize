8c6b45dc27ddfbdc78d97561a2b459d0969d70a7
Bill Dufresne <371570273@qq.com>
2017-03-16 17:02:13 +0800

    bug_fixed

--- DemoAllTrainingImg.m
+++ DemoAllTrainingImg.m
@@ -23,29 +23,35 @@ for i = 1:27
     Trimap = imread(path);
 
     % call ComprehensiveSamplingMatting .
+    tic;
     EstimatedAlpha = ComprehensiveSamplingMatting(I, Trimap) ; 
+    toc;
     %figure(1),imshow(EstimatedAlpha);
     path = strcat('.\res_training_lowres\',name_prefix,'_Org.png');
     fprintf('Now writing: %s...\n',path);
     imwrite(EstimatedAlpha,path);
 
     %Downsize and upsize test
+    tic;
     IDsz = Downsize(I);
     TrimapDsz = Downsize(Trimap);
 
     EstimatedAlphaDsz = ComprehensiveSamplingMatting(IDsz, TrimapDsz) ;
     EstimatedAlphaDsz = Upsize(EstimatedAlphaDsz);
+    toc;
     %figure(2),imshow(EstimatedAlphaDsz);
     path = strcat('.\res_training_lowres\',name_prefix,'_Dsz.png');
     fprintf('Now writing: %s...\n',path);
     imwrite(EstimatedAlphaDsz,path);
 
     %Avr downsize and upsize test
+    tic;
     IDsz_avr = Downsize_avr(I);
     TrimapDsz_avr = Downsize(Trimap);
 
     EstimatedAlphaDsz_avr = ComprehensiveSamplingMatting(IDsz_avr, TrimapDsz_avr) ;
     EstimatedAlphaDsz_avr = Upsize(EstimatedAlphaDsz_avr);
+    toc;
     %figure(3),imshow(EstimatedAlphaDsz);
     path = strcat('.\res_training_lowres\',name_prefix,'_Dsz_avr.png');
     fprintf('Now writing: %s...\n',path);
--- Downsize_avr.m
+++ Downsize_avr.m
@@ -2,8 +2,8 @@ function DszImg = Downsize_avr (IOrg)
 
            [row,col,layer] = size(IOrg);
            
-           for i = 1:2:row
-              for j = 1:2:col
+           for i = 1:2:row-1
+              for j = 1:2:col-1
                   for k = 1:layer % k=1,2,3表示分别对R,G,B三通道
                       pixel_avr = (uint16(IOrg(i,j,k))+uint16(IOrg(i,j+1,k))+uint16(IOrg(i+1,j,k))+uint16(IOrg(i+1,j+1,k)))/4;
                       ResImg((i+1)/2,(j+1)/2,k) = uint8(pixel_avr);
