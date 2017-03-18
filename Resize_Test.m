67d838490c9d7f52f556e19b48e9d4299ccbf4bd
Bill Dufresne <371570273@qq.com>
2017-03-18 16:18:59 +0800

    mse result

--- Resize_Test.m
+++ Resize_Test.m
@@ -12,18 +12,18 @@ Trimap = imread('Trimap.png');
 figure(1),imshow(I);
 tempImg = Downsize_avr(I);
 figure(2),imshow(tempImg);
-%figure(3),imshow(Upsize(tempImg));
+figure(3),imshow(Upsize(I,tempImg));
 %imwrite(subimage1,strcat('F:\jinwei\pictureextacted\',[num2str(k),'1.bmp']));
 %imwrite(Upsize(tempImg),'.\res_training_lowres\test.png');
 
-t1=clock;
-for i = 1:5
-    imwrite(Upsize(tempImg),strcat('.\res_training_lowres\','test',[num2str(i),'.png']));
-end
+% t1=clock;
+% for i = 1:5
+%     imwrite(Upsize(tempImg),strcat('.\res_training_lowres\','test',[num2str(i),'.png']));
+% end
 
 %write into file
-fid=fopen('file_test.txt','a');
-t2=clock;
-time = etime(t2,t1);
-fprintf(fid,'1stline: %fs\n',time);
-fclose(fid);
+% fid=fopen('file_test.txt','a');
+% t2=clock;
+% time = etime(t2,t1);
+% fprintf(fid,'1stline: %fs\n',time);
+% fclose(fid);
